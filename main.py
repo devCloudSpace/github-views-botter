9683023166c96a7398179d15bd2e675f182c2680
Woosh <51231397+LetMeWoosh@users.noreply.github.com>
2021-09-12 15:03:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from proxy_requests import ProxyRequests
 import time
 import os
 
-url = input('URL: ')
-reqcount = int(input('Number Of Requests To Send: '))
-uagent = input('User Agent: ')
+url = input('URL > ')
+reqcount = int(input('How many requests? > '))
+uagent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
 
-os.system('cls')
 print('Sending Requests, This May Take A While')
 for i in range(reqcount):
     headers = {
@@ -15,3 +14,5 @@ for i in range(reqcount):
     r = ProxyRequests(url)
     r.set_headers(headers)
     r.get_with_headers()
+    print(r.get_status_code())
+    print(r.get_proxy_used())  
