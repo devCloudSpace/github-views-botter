d0c68fcce41ba6d6894a0298cbf679cb8b7694a1
Woosh <51231397+LetMeWoosh@users.noreply.github.com>
2021-09-12 14:35:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import requests, threading, os
 
-url = input('raw link: ')
-amount = input('enter the amount of threads (go for 300): ')
+url = input('Link > ')
+amount = input('Threads > ')
 
 def thread():
-  while True:  
+  while True:
+     s = requests.session()
+     proxy = set()
+     with open("proxies.txt", "r") as f:
+     file_lines1 = f.readlines()
+     for line1 in file_lines1:
+         proxy.add(line1.strip())
+proxies = {
+    'http': 'http://'+line1
+    }
+
      sent = 0
-     r = requests.get(url)
+     r = requests.get(url, proxies=proxies)
      if r.status_code == 403:
-         print('Invalid URL')
+         print('Invalid URL/Proxy blocked')
      else:
       pass
       sent += 1
-      print(f'Views sent: %s' % sent)
+      print(f'%s requests have been sent' % sent)
 
 for _ in range(int(amount)):
     t = threading.Thread(target=thread)
